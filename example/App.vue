ae4629c7bd461e712a4a925cd25343dfc4e0afe0
Bobbie Goede <bobbiegoede@gmail.com>
2024-05-15 09:52:51 +0200

    chore: use pnpm and node 18 (#198)
    * refactor: use pnpm
    
    * ci: enable corepack
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: kazuya kawaguchi <kawakazu80@gmail.com>

--- example/App.vue
+++ example/App.vue
@@ -3,15 +3,8 @@
   <p v-t="{ path: hello, locale: 'en' }"></p>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue'
-import { useI18n } from 'vue-i18n'
 
-export default {
-  name: 'App',
-  setup() {
-    const hello = ref('hello')
-    return { hello, ...useI18n() }
-  }
-}
+const hello = ref('hello')
 </script>
