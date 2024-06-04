dd1273b924aeb5fc6972d5e1af0e9e53d9164446
kazuya kawaguchi <kawakazu80@gmail.com>
2024-06-04 15:33:45 +0900

    reproduction

--- test/ssr.test.ts
+++ test/ssr.test.ts
@@ -74,3 +74,41 @@ test('v-t: legacy', async () => {
   app.use(i18n)
   expect(await renderToString(app)).toMatch(`<div>2 bananas</div>`)
 })
+
+test('repro', async () => {
+  const i18n = createI18n({
+    legacy: false,
+    locale: 'ja',
+    messages: {}
+  })
+
+  const transformVT = transformVTDirective({})
+  const source = `<div v-t="{ path: 'dessert', locale: 'en', plural: 2, args: { name: 'banana' } }"/>`
+  const { code } = compile(source, {
+    mode: 'function',
+    directiveTransforms: { t: transformVT }
+  })
+  // prettier-ignore
+  // eslint-disable-next-line @typescript-eslint/no-implied-eval, @typescript-eslint/ban-types
+  const render = Function('require', 'Vue', code)(require, runtimeDom) as Function
+  const App = defineComponent({
+    setup() {
+      const { t: translation } = useI18n({
+        locale: 'en',
+        inheritLocale: false,
+        messages: {
+          en: {
+            apple: 'no apples | one apple | {count} apples',
+            banana: 'no bananas | {n} banana | {n} bananas',
+            dessert: 'I eat @:{name}!'
+          }
+        }
+      })
+      return { translation }
+    },
+    ssrRender: render
+  })
+  const app = createSSRApp(App)
+  app.use(i18n)
+  expect(await renderToString(app)).toMatch(`<div>I eat 2 bananas!</div>`)
+})
