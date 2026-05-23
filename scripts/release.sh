c347de28c2cc167968fa8eaaae7535753a53bef6
kazuya kawaguchi <kawakazu80@gmail.com>
2026-05-23 19:16:22 +0900

    security: enable npm trusted publishing (#337)

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,19 +2,9 @@
 
 set -xe
 
-# Check nightly release
 if [[ ! -z ${NIGHTLY_RELEASE} ]] ; then
-  npx --no-install tsx ./scripts/bump-nightly
+  pnpm exec tsx ./scripts/bump-nightly
 fi
 
-# Update token
-if [[ ! -z ${NPM_TOKEN} ]] ; then
-  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
-  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
-  echo "always-auth=true" >> ~/.npmrc
-  npm whoami
-fi
-
-# Release packages
 echo "Publishing"
-pnpm publish --access public --no-git-checks --tag next
+npm publish --access public --tag next
